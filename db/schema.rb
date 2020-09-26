c9a0574bafd2ccab4603b4c186d27a1e6e4dcc6a
Jade Lynn Masker <jadelynn@masker.family>
2020-09-25 20:45:06 -0400

    run rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200926004152) do
+ActiveRecord::Schema.define(version: 20200926004428) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text     "body"
+    t.integer  "user_id"
+    t.integer  "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "images", force: :cascade do |t|
     t.string   "src"
