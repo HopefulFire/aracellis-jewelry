d4cced098b25e9795e5ffa55819d80ba376289ec
Jade Lynn Masker <jadelynn@masker.family>
2020-09-27 15:47:50 -0400

    made it so comments don't have to be unique, made users/home contain comments and posts if available

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,5 @@
 class Comment < ActiveRecord::Base
-  validates :body, uniqueness: true
-  validates :user_id, :post_id, presence: true
+  validates :body, :user_id, :post_id, presence: true
 
   belongs_to :user
   belongs_to :post
