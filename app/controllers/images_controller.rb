a7b380ff2c47a01bb3ed6253be93ba2fe0b2a9ae
Jade Lynn Masker <jadelynn@masker.family>
2020-09-29 16:33:46 -0400

    run rubocop-rails

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,37 +1,36 @@
 class ImagesController < ApplicationController
-
   # GET: /images
-  get "/images" do
+  get '/images' do
     erb :"/images/index.html"
   end
 
   # GET: /images/new
-  get "/images/new" do
+  get '/images/new' do
     erb :"/images/new.html"
   end
 
   # POST: /images
-  post "/images" do
-    redirect "/images"
+  post '/images' do
+    redirect '/images'
   end
 
   # GET: /images/5
-  get "/images/:id" do
+  get '/images/:id' do
     erb :"/images/show.html"
   end
 
   # GET: /images/5/edit
-  get "/images/:id/edit" do
+  get '/images/:id/edit' do
     erb :"/images/edit.html"
   end
 
   # PATCH: /images/5
-  patch "/images/:id" do
-    redirect "/images/:id"
+  patch '/images/:id' do
+    redirect '/images/:id'
   end
 
   # DELETE: /images/5/delete
-  delete "/images/:id/delete" do
-    redirect "/images"
+  delete '/images/:id/delete' do
+    redirect '/images'
   end
 end
