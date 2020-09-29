24188cce417034c0cc9c584d53ebd7a9f9bab3bd
Jade Lynn Masker <jadelynn@masker.family>
2020-09-29 16:17:22 -0400

    some validations and associations

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,2 +1,6 @@
 class Image < ActiveRecord::Base
+  validates :src, presence: true
+
+  belongs_to :user
+  belongs_to :post
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,7 @@
 class Post < ActiveRecord::Base
+  validates :title, :body, :user_id, presence: true
+
+  belongs_to :user
+  has_many :comments
+  has_many :images
 end
