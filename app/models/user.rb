85f30e682025ed7dde711c4b27bc7ee660f5dcf1
Jade Lynn Masker <jadelynn@masker.family>
2020-09-29 14:42:02 -0400

    bundle install, rake db:migrate, and fleshed out user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ActiveRecord::Base
+  validates :username, :email_address, uniqueness: true
+  has_secure_password
+
+  has_many :comments
+  has_many :posts
+  has_many :images
 end
--- db/migrate/20200929183123_create_users.rb
+++ db/migrate/20200929183123_create_users.rb
@@ -3,7 +3,7 @@ class CreateUsers < ActiveRecord::Migration
     create_table :users do |t|
       t.string :username
       t.string :email_address
-      t.bool :is_admin
+      t.boolean :is_admin
       t.string :password_digest
 
       t.timestamps null: false
