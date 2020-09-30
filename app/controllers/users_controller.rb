b0afaf7019d6b9a54cd06070941c6e8ba9a388e6
Jade Lynn Masker <jadelynn@masker.family>
2020-09-30 13:04:32 -0400

    fix another bug

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   # GET: /users
   get '/users' do
-    @users = User.all.reversed
+    @users = User.all.reverse
     erb :"/users/index.html"
   end
 
