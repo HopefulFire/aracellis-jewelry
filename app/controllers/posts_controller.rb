81290fce5423a29fb6b72278f59dec841683bda5
Jade Lynn Masker <jadelynn@masker.family>
2020-10-01 14:47:53 -0400

    some stuff I should have made into smaller commits

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,17 +9,43 @@ class PostsController < ApplicationController
   # GET: /posts/new
   get '/posts/new' do
     @user = User.find_by(id: session[:user_id])
-    erb :"/posts/new.html"
+    if @user&.is_admin
+      erb :"/posts/new.html"
+    else
+      @message = 'You must log in as an admin to submit a post'
+      @link = '/users/login'
+      erb :"/status/failure.html"
+    end
   end
 
   # POST: /posts
   post '/posts' do
-    redirect '/posts'
+    @user = User.find_by(id: session[:user_id])
+    if @user&.is_admin
+      @post = Post.new
+      @post.title = params[:title]
+      @post.body = params[:body]
+      @post.user = @user
+      if @post.save
+        @message = 'Successfully created your post!'
+        @link = "/posts/#{@post.id}"
+        erb :"/status/success.html"
+      else
+        @message = "#{@posts.errors.messages.keys.first}: #{@posts.errors.messages.values.first.first}"
+        @link = '/posts/new'
+        erb :"/status/failure.html"
+      end
+    else
+      @message = 'You must log in as an admin to submit a post'
+      @link = '/users/login'
+      erb :"/status/failure.html"
+    end
   end
 
   # GET: /posts/5
   get '/posts/:id' do
     @user = User.find_by(id: session[:user_id])
+    @post = Post.find_by(id: params[:id])
     erb :"/posts/show.html"
   end
 
